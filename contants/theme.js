75dd8afe2a3a461b8b785e9228d971e56478dd59
Unn Byambaa <unnaidan@gmail.com>
2020-10-09 17:33:12 +0800

    refactor theme.js

--- contants/theme.js
+++ contants/theme.js
@@ -1,6 +1,85 @@
 export default {
-    primary: {
-        main: '#0F4C81',
-        contrastText: '#FFFFFF'
+    shape: {
+        borderRadius: 4,
+    },
+    typography: {
+        h1: {
+            fontWeight: 700,
+            fontSize: 36
+        },
+        h2: {
+            fontWeight: 700,
+            fontSize: 32
+        },
+        h3: {
+            fontWeight: 700,
+            fontSize: 30
+        },
+        h4: {
+            fontWeight: 700,
+            fontSize: 26
+        },
+        h5: {
+            fontWeight: 700,
+            fontSize: 22
+        },
+        h6: {
+            fontWeight: 700,
+            fontSize: 18
+        },
+        s1: {
+            fontWeight: 700,
+            fontSize: 16
+        },
+        s2: {
+            fontWeight: 700,
+            fontSize: 14
+        },
+        p1: {
+            fontWeight: 400,
+            fontSize: 16
+        },
+        p2: {
+            fontWeight: 400,
+            fontSize: 14
+        },
+        caption: {
+            fontWeight: 400,
+            fontSize: 12
+        }
+    },
+    colors: {
+        divider: {
+            main: '#E6E6E7'
+        },
+        text: {
+            default: '#02090F',
+            hint: '#9A9D9F',
+            control: '#FFFFFF'
+        },
+        primary: {
+            main: '#0F4C81',
+            contrastText: '#FFFFFF'
+        },
+        basic: {
+            main: '#F6F6F6',
+            contrastText: '#02090F'
+        },
+        success: {
+            main: '#0F842F',
+            contrastText: '#FFFFFF'
+        },
+        info: {
+            main: '#1A4AB2',
+            contrastText: '#FFFFFF'
+        },
+        warning: {
+            main: '#E27900',
+            contrastText: '#FFFFFF'
+        },
+        error: {
+            main: '#9B1515',
+            contrastText: '#FFFFFF'
+        }
     }
 }
